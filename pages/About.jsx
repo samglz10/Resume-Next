adffb22f05670ea69d0914a3deeb728f1dacaad1
Samuel Gonzalez <samglz10@gmail.com>
2022-11-09 21:01:20 -0500

    fixed minor html text issues

--- pages/About.jsx
+++ pages/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
                         </p>
                     </div>
                     <div>
-                        <p className='text-lg px-4 truncate sm:overflow-auto'>
+                        <p className='text-lg px-4  sm:overflow-auto'>
                         I have a Bachelors Degree in Criminal Justice and with a minor in Spanish. Through my career I have provided excellent support to various government agencies such as the Federal Bureau of Investigation (FBI), 
                         Department of State (DOS), Department of Justice (DOJ),and other government contractors. I have conducted in-depth of Background Investigations for those who seek employment with the U.S government. 
                         I have administered National Security and Public Trust Interviews and produced in depth Report of Investigations (ROI) to their respective agencies. I have advance in my career by providing analytical case reviews 
--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -7,14 +7,14 @@ const Contact = () => {
         <form method="POST" action="https://getform.io/f/fb384145-d194-4702-9fab-7de5bc9ce9c3" className="flex flex-col h-full w-full px-8 justify-center items-center sm:grow">
         <div className="pb-8 flex flex-col items-center">
                 <p className="text-4xl font-bold inline border-b-sky-500 border-b-4 text-white ">Contact</p>
-                <p className="text-white py-4"> Submit the form to or you can email me @</p>
+                <p className="text-white py-4"> Submit the form to or you can email me at the bottom 📧 icon!</p>
             </div>
             <div className="flex flex-col justify-center text-center content-center w-1/2 place-content-center">
               <input className="p-2 flex flex-wrap bg-slate-500" type="text" placeholder="Name" name="name"/>
               <input className="my-4 p-2 bg-slate-500" type="text" placeholder="Email" name="email"/>
               <textarea className="bg-slate-500 text-justify" name="message" row="10" placeholder="Message"></textarea>
             </div>
-            <button className=" flex-col justify-center content-center items-center w-1/2 text-white border-2  border-sky-500 px-4 py-3 my-8 flex  hover:bg-gradient-to-r from-sky-500 to-blue-800  ">Let's Collaborate</button>
+            <button className=" flex-col justify-center content-center items-center w-1/2 text-white border-2  border-sky-500 px-4 py-3 my-8 flex  hover:bg-gradient-to-r from-sky-500 to-blue-800  ">Hit Me Up!</button>
         </form>
       </div>
     )
